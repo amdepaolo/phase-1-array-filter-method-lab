64295faa530883f977c1383f5c7b7d5df1665d57
Adam DePaolo <amdepaolo@gmail.com>
2022-03-24 18:55:20 -0400

    commit tree a2ac4f8f5db61e7e0ea45e5ccfe419f1291ea39a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching (array, name){
+    let resultNames = array.filter(
+        function(item){
+            if (name.toUpperCase() === item.toUpperCase())
+            return item
+        });
+    return resultNames
+};
+
+function fuzzyMatch(array, name){
+    let fuzzyResults = array.filter(
+        function(item){
+            if (name[0].toUpperCase() === item[0].toUpperCase())
+            return item
+        });
+    return fuzzyResults
+};
+
+function matchName( array, string){
+    let matches = array.filter (function(info){
+        return info.name === string 
+    })
+    return matches
+}
